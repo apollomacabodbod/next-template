957779e497b6a879b8173a0b53136810d4123e7b
apollo <apollomacabodbod11@gmail.com>
2024-05-04 07:06:25 +0800

    Initial commit

---
+++ app/Pages/Home/index.jsx
@@ -0,0 +1,28 @@
+
+
+
+
+import Section from "Components/Section"
+
+
+export default function Home(){
+
+
+  return (
+
+    <>
+
+
+      <div className="max-w-[1200px] mx-auto border-[2px] border-[#FF0000]" >
+
+        <Section/>
+
+
+      </div>
+
+     
+        
+        
+    </>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'min': '800px' , 'max': '900px'},
+        // => @media (max-width: 1023px) { ... }
+  
+      }
+
+      
+    },
+  },
+  plugins: [],
+};
+export default config;
