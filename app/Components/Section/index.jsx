4f811dc99a0ed41778205c3f2b089c1e713faf24
apollo <apollomacabodbod11@gmail.com>
2024-05-27 12:32:48 +0800

    add more xs: classes

--- app/Components/Section/index.jsx
+++ app/Components/Section/index.jsx
@@ -10,7 +10,7 @@ export default function Section(){
   
   
       <ul className="flex flex-wrap gap-[50px] mt-[80px] border-[2px] border-[#EE4E4E] ml-[80px] mr-[80px] ">
-        <li className="min-w-[150px] flex-[1] border-[2px] border-[#EE4E4E] s:min-w-[calc(100%-20px)] xs:hidden" ><p className="truncate p-[4px]">Item 1</p></li>
+        <li className="min-w-[280px] flex-[1] border-[2px] border-[#EE4E4E] s:min-w-[calc(100%-20px)] xs:border-[#524C42] xs:text-green-500" ><p className="truncate p-[4px]">Item 1</p></li>
         <li className="min-w-[150px] flex-[1] border-[2px] border-[#EE4E4E] s:min-w-[calc(100%-20px)] "><p className="truncate p-[4px]">Item 2</p></li>
         <li className="min-w-[150px] flex-[1] border-[2px] border-[#EE4E4E] s:min-w-[calc(100%-20px)]"><p className="truncate p-[4px]">Item 3</p></li>
         <li className="min-w-[150px] flex-[1] border-[2px] border-[#EE4E4E] s:min-w-[calc(100%-20px)]"><p className="truncate p-[4px]">Item 4</p></li>
