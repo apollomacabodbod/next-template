6008e3ffb2032e0dab917a0056f4333367701c1a
apollo <apollomacabodbod11@gmail.com>
2024-07-20 19:03:14 +0800

    added yarn lock file

--- app/Pages/Home/index.jsx
+++ app/Pages/Home/index.jsx
@@ -15,7 +15,7 @@ export default function Home(){
     <>
 
 
-      <div className="max-w-[1200px] mx-auto border-[2px] border-[#EE4E4E]" >
+      <div className="max-w-[1200px] mx-auto border-[2px] border-[#50B498]" >
 
         <Section/>
 
